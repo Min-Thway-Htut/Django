44318924944d570ecfac8ab7cc8ee2e8cffcb8c5
Min-Thway-Htut <minthwayhtut568@gmail.com>
2025-01-22 15:48:20 +0100

    Query

--- members/views.py
+++ members/views.py
@@ -23,10 +23,10 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing(request):
-    mymembers = Member.objects.all().values()
+    mydata = Member.objects.all().order_by('firstname').values()
     template = loader.get_template('template.html')
     context = {
-        'mymembers': mymembers,
+        'mymembers': mydata,
     }
     return HttpResponse(template.render(context, request))
 
