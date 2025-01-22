86e3ff10defb03f89886c998b522639dfa4cc960
Min-Thway-Htut <minthwayhtut568@gmail.com>
2025-01-22 14:04:28 +0100

    Another Update

--- members/views.py
+++ members/views.py
@@ -23,8 +23,16 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing(request):
+    mymembers = Member.objects.all().values()
     template = loader.get_template('template.html')
     context = {
-        'fruits': ['Apple', 'Banana', 'Cherry'],
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def example(request):
+    template = loader.get_template('firstname.html')
+    context = {
+        'firstname': 'Linus',
     }
     return HttpResponse(template.render(context, request))
